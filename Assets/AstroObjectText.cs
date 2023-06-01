f529ca885696a4732f6f29549cfaee98d915a274
Kevin Khuu <kkhuu@uw.edu>
2023-06-01 14:09:58 -0700

    Added name text that tracks each celestial

---
+++ Assets/AstroObjectText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// AstroObjectText repeatedly moves a text object so that it tracks a given object on the screen
+public class AstroObjectText : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Transform objectToTrack;
+    public TMP_Text textUI;
+    public float offset = 20;
+
+    void Update()
+    {
+        // Get position of object and convert to position of object in camera POV
+        Vector3 objectPosition = objectToTrack.position;
+
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(objectPosition);
+        screenPosition.z = 0;
+
+        screenPosition.y += offset;
+
+        // Set position of text
+        textUI.rectTransform.position = screenPosition;
+    }
+}
